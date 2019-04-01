ff0ff5b32f35a43f6d6ff2e43439b6d39c42d802
paulinajade <paulinajadem@hotmail.com>
2019-04-01 11:56:09 -0700

    change file names

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//STATE VARIABLE
+
+var comedymovie = {
+  title: "",
+  year:"",
+  rating: "",
+  description: ""
+}
+
+var romancemovie = {
+  title: "",
+  year:"",
+  rating: "",
+  description: ""
+}
+
+var horrormovie = {
+  title: "",
+  year:"",
+  rating: "",
+  description: ""
+}
+
+var dramamovie = {
+  title: "",
+  year:"",
+  rating: "",
+  description: ""
+}
+
+//PROXY SETUP
+
+
+//CHANGE STATE FUNCTIONS
+
+
+//CHANGE UI FUNCTIONS
