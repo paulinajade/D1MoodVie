ddb857250ece746e95891e3bcb5e59330cb397ea
DeriqDanois <deriqdanois@gmail.com>
2019-05-11 19:26:03 -0700

    yoo

--- js/app.js
+++ js/app.js
@@ -1,7 +1,26 @@
+
+// Clickable Input
+var label1 = document.getElementsByClassName("label")
+var label2 = document.getElementsByClassName("label2")
+var label3 = document.getElementsByClassName("thirdlabel")
+
+// UI BOX COLORS
+var box1 = document.getElementsByClassName("box")
+var box2 = document.getElementsByClassName("box2")
+var box3 = document.getElementsByClassName("box3")
+
+// box1[0].style.backgroundColor = "red"
+// box2[0].style.backgroundColor = "red"
+// box3[0].style.backgroundColor = "red"
+
+
 var pkg = {
   mood:"",
   genre:"",
   era:"",
+  bgone: "",
+  bgtwo: "",
+  bgthree: ""
 }
 
 var storedPackage = localStorage.getItem("package");
@@ -12,48 +31,81 @@ if(storedPackage){
     mood:"Happy",
     genre:"Comedy",
     era:"Before-2000s",
+    bgone: "",
+    bgtwo: "",
+    bgthree: ""
   }
 }
 
 var handler = {
   set:function(obj, prop, value){
     obj[prop] = value; 
-     //mood
-    /*
-     if (prop == "mood"){
-      changeMood(val, "");
+   
+     if (value == "Happy"){
+      changeBGHappy(value);
+     }
+     if (value == "Sad"){
+      changeBGSad(value);
+     }
+     if (value == "Brave"){
+      changeBGBrave(value);
+     }
+     if (value == "Loving"){
+       changeBGLoving(value);
+     }
+
+     //genre 
+
+     if (value == "Comedy"){
+      changeBGComedy(value);
+     }
+     if (value == "Horror/Thriller"){
+      changeBGHorror(value);
+     }
+     if (value == "Romance"){
+      changeBGBRomance(value);
+     }
+     if (value == "Drama"){
+       changeBGDrama(value);
+     }
+
+     if (value == "Before-2000s"){
+       changeEra1UI(value)
+     }
+     if (value == "After-2000s"){
+       changeEra2UI(value)
      }
     
-     if(prop == "mood"){
-     changeMood(val, "Sad");
-     }
-     if(prop == "mood"){
-     changeMood(val, "Brave");
-     }
-     if(prop == "mood"){
-     changeMood(val, "Loving");
-     } 
-     //pref genre
-     if(prop == "genre"){
-     changeGenre(val, "Comedy");
-     }
-     if(prop == "genre"){
-     changeGenre(val, "Horror/Thriller");
-     }
-     if(prop == "genre"){
-     changeGenre(val, "Romance");
-     }
-     if(prop == "genre"){
-     changeGenre(val, "Drama");
-     }
-     // pref era
-     if(prop == "era"){
-       ChangeEra == (val, "Before-2000s")
-     }
-     if(prop == "era"){
-       ChangeEra == (val, "After-2000s")
-     }
-     */
+    //  if(prop == "mood"){
+    //  changeMood(val, "Sad");
+    //  }
+    //  if(prop == "mood"){
+    //  changeMood(val, "Brave");
+    //  }
+    //  if(prop == "mood"){
+    //  changeMood(val, "Loving");
+    //  } 
+    //  //pref genre
+    //  if(prop == "genre"){
+    //  changeGenre(val, "Comedy");
+    //  }
+    //  if(prop == "genre"){
+    //  changeGenre(val, "Horror/Thriller");
+    //  }
+    //  if(prop == "genre"){
+    //  changeGenre(val, "Romance");
+    //  }
+    //  if(prop == "genre"){
+    //  changeGenre(val, "Drama");
+    //  }
+    //  // pref era
+    //  if(prop == "era"){
+    //    ChangeEra == (val, "Before-2000s")
+    //  }
+    //  if(prop == "era"){
+    //    ChangeEra == (val, "After-2000s")
+    //  }
+    //  
   }
 }
 
@@ -64,23 +116,134 @@ var proxPkg = new Proxy(pkg, handler);
 //Mood input
 function changeMood(el){
   proxPkg.mood = el.value;
+  if(el.value === "Happy"){
+  proxPkg.bgone = "#2b5ddb"
+  } 
+
+  if(el.value === "Sad"){
+  proxPkg.bgone = "#2b5ddb"
+  } 
+
+  if(el.value === "Brave"){
+  proxPkg.bgone = "#2b5ddb"
+  } 
+  if(el.value === "Loving"){
+  proxPkg.bgone = "#2b5ddb"
+  }
   console.log(proxPkg.mood);
   save();
 }
 
+//Genre Input
+
 function changeGenre(el){
   proxPkg.genre = el.value;
-console.log(proxPkg.genre);
-save();
+  if(el.value === "Comedy"){
+    proxPkg.bgtwo = "#2b5ddb"
+    } 
+
+    if(el.value === "Horror/Thriller"){
+    proxPkg.bgtwo = "#2b5ddb"
+    } 
+  
+    if(el.value === "Romance"){
+    proxPkg.bgtwo = "#2b5ddb"
+    } 
+    if(el.value === "Drama"){
+    proxPkg.bgtwo = "#2b5ddb"
+    }
+    console.log(proxPkg.genre);
+    save();
 }
+
+// Era input
 
 function changeEra(el){
   proxPkg.era = el.value;
+  if (el.value === "Before-2000s"){
+    proxPkg.bgthree = "#2b5ddb"
+  }
+  if (el.value === "After-2000s"){
+    proxPkg.bgthree = "#2b5ddb"
+  }
+  
 console.log(proxPkg.era);
 save();
 }
 
+//UI changes
+
+function changeBGHappy(val){
+  box1[0].style.backgroundColor = proxPkg.bgone
+  box1[1].style.backgroundColor = ""
+  box1[2].style.backgroundColor = ""
+  box1[3].style.backgroundColor = ""
+  
+}
+function changeBGSad(val){
+  box1[1].style.backgroundColor = proxPkg.bgone
+  box1[0].style.backgroundColor = ""
+  box1[2].style.backgroundColor = ""
+  box1[3].style.backgroundColor = ""
+
+}
+function changeBGBrave(val){
+  box1[2].style.backgroundColor = proxPkg.bgone
+  box1[1].style.backgroundColor = ""
+  box1[0].style.backgroundColor = ""
+  box1[3].style.backgroundColor = ""
+}
+function changeBGLoving(val){
+  box1[3].style.backgroundColor = proxPkg.bgone
+  box1[2].style.backgroundColor = ""
+  box1[1].style.backgroundColor = ""
+  box1[0].style.backgroundColor = ""
+}
+
+// genre bg
+
+function changeBGComedy(val){
+  box2[0].style.backgroundColor = proxPkg.bgtwo
+  box2[1].style.backgroundColor = ""
+  box2[2].style.backgroundColor = ""
+  box2[3].style.backgroundColor = ""
+  
+}
+function changeBGHorror(val){
+  box2[1].style.backgroundColor = proxPkg.bgtwo
+  box2[0].style.backgroundColor = ""
+  box2[2].style.backgroundColor = ""
+  box2[3].style.backgroundColor = ""
+
+}
+function changeBGBRomance(val){
+  box2[2].style.backgroundColor = proxPkg.bgtwo
+  box2[1].style.backgroundColor = ""
+  box2[0].style.backgroundColor = ""
+  box2[3].style.backgroundColor = ""
+}
+function changeBGDrama(val){
+  box2[3].style.backgroundColor = proxPkg.bgtwo
+  box2[2].style.backgroundColor = ""
+  box2[1].style.backgroundColor = ""
+  box2[0].style.backgroundColor = ""
+}
+
+// Era bg 
+
+function changeEra1UI(){
+  box3[0].style.backgroundColor = proxPkg.bgthree;
+  box3[1].style.backgroundColor = "";
+}
+function changeEra2UI(){
+  box3[1].style.backgroundColor = proxPkg.bgthree;
+  box3[0].style.backgroundColor = "";
+}
+
 console.log(proxPkg)
+
+
+
 
 function changePage(){
   
@@ -163,3 +326,4 @@ function save(){
   localStorage.setItem("package",JSON.stringify(proxPkg));
   
 }
+
